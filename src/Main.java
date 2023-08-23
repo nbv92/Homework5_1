4c371f562eed7114a133eb5ff6282e5467369b94
Timofey <timofey.sharipov@gmail.com>
2023-08-23 22:39:25 +0300

    second_hand

--- src/Main.java
+++ src/Main.java
@@ -5,59 +5,63 @@ public class Main {
         task3();
         task4();
     }
+
+    static int[] arrayInt = new int[] { 1, 2, 3 };
+    static double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
+    static String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
     public static void task1() {
         System.out.println("Task1");
-        int[] arrayInt = new int[] { 1, 2, 3 };
-        double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
-        String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
+        //int[] arrayInt = new int[] { 1, 2, 3 };
+        //double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
+        //String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
 
     }
     public static void task2() {
         System.out.println("Task2\n");
-        int[] arrayInt = new int[] { 1, 2, 3 };//Как сделать так чтобы переменные из задачи 1 были видно в задаче 2?
-        double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
-        String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
-        for (int i = 0; i<=arrayInt.length;i++){
+        //int[] arrayInt = new int[] { 1, 2, 3 };
+        //double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
+        //String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
+        for (int i = 0; i <= arrayInt.length; i++) {
             if (i == arrayInt.length - 1) {
                 System.out.println(arrayInt[i]);
                 break;
             }
-            System.out.printf (arrayInt[i]+", ");
+            System.out.printf (arrayInt[i] + ", ");
 
         }
         System.out.println("");
-        for (int i = 0; i<=arrayDouble.length;i++){
+        for (int i = 0; i<=arrayDouble.length; i++){
             if (i == arrayDouble.length - 1) {
                 System.out.println(arrayDouble[i]);
                 break;
             }
-            System.out.print(arrayDouble[i]+", ");
+            System.out.print(arrayDouble[i] + ", ");
         }
         System.out.println("");
-        for (int i = 0; i<=arrayDouble.length;i++){
+        for (int i = 0; i <= arrayDouble.length; i++){
             if (i == arrayString.length - 1) {
                 System.out.println(arrayString[i]);
                 break;
             }
-            System.out.print(arrayString[i]+", ");
+            System.out.print(arrayString[i] + ", ");
         }
         System.out.println("");
     }
     public static void task3() {
         System.out.println("Task3\n");
-        int[] arrayInt = new int[] { 1, 2, 3 };
-        double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
-        String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
-        for (int i = arrayInt.length;i>=0;i--){
+        //int[] arrayInt = new int[] { 1, 2, 3 };
+        //double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
+        //String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
+        for (int i = arrayInt.length; i >= 0; i--){
             if (i == 0) {
                 System.out.println(arrayInt[i]);
                 break;
             }
-            System.out.printf (arrayInt[i]+", ");
+            System.out.print (arrayInt[i] + ", ");
 
         }
         System.out.println("");
-        for (int i = arrayDouble.length;i>=0;i--) {
+        for (int i = arrayDouble.length; i >= 0; i--) {
             if (i == 0) {
                 System.out.println(arrayDouble[i]);
                 break;
@@ -77,13 +81,17 @@ public class Main {
     }
     public static void task4() {
         System.out.println("Task4");
-        int[] arrayInt = new int[] { 1, 2, 3 };
-        for(int i:arrayInt){
+        //int[] arrayInt = new int[] { 1, 2, 3 };
+
+        for(int i: arrayInt){
+            if (arrayInt[i] % 2 != 0) {
+                arrayInt[i] += 1;
+            }
             if (i == arrayInt.length - 1) {
                 System.out.println(arrayInt[i]+1);
                 break;
             }
-            System.out.print(arrayInt[i]+1+", ");
+            System.out.print(arrayInt[i]", ");
         }
     }
 
