c6ccd7309bffa0f9296153ad9f8c758e117a5c1f
Timofey <timofey.sharipov@gmail.com>
2023-08-25 19:13:51 +0300

    second_hand

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ public class Main {
         task4();
     }
 
-    static int[] arrayInt = new int[] { 1, 2, 3 };
-    static double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
-    static String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
+    static int[] arrayInt = new int[]{1, 2, 3};
+    static double[] arrayDouble = new double[]{1.57, 7.654, 9.986};
+    static String[] arrayString = new String[]{"FOR ", "THE ", "EMPEROR!"};
+
     public static void task1() {
         System.out.println("Task1");
         //int[] arrayInt = new int[] { 1, 2, 3 };
@@ -16,6 +17,7 @@ public class Main {
         //String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
 
     }
+
     public static void task2() {
         System.out.println("Task2\n");
         //int[] arrayInt = new int[] { 1, 2, 3 };
@@ -26,11 +28,11 @@ public class Main {
                 System.out.println(arrayInt[i]);
                 break;
             }
-            System.out.printf (arrayInt[i] + ", ");
+            System.out.printf(arrayInt[i] + ", ");
 
         }
         System.out.println("");
-        for (int i = 0; i<=arrayDouble.length; i++){
+        for (int i = 0; i <= arrayDouble.length; i++) {
             if (i == arrayDouble.length - 1) {
                 System.out.println(arrayDouble[i]);
                 break;
@@ -38,7 +40,7 @@ public class Main {
             System.out.print(arrayDouble[i] + ", ");
         }
         System.out.println("");
-        for (int i = 0; i <= arrayDouble.length; i++){
+        for (int i = 0; i <= arrayDouble.length; i++) {
             if (i == arrayString.length - 1) {
                 System.out.println(arrayString[i]);
                 break;
@@ -47,17 +49,18 @@ public class Main {
         }
         System.out.println("");
     }
+
     public static void task3() {
         System.out.println("Task3\n");
         //int[] arrayInt = new int[] { 1, 2, 3 };
         //double[] arrayDouble = new double[] { 1.57, 7.654, 9.986};
         //String[] arrayString = new String[] {"FOR ","THE ","EMPEROR!"};
-        for (int i = arrayInt.length; i >= 0; i--){
+        for (int i = arrayInt.length; i >= 0; i--) {
             if (i == 0) {
                 System.out.println(arrayInt[i]);
                 break;
             }
-            System.out.print (arrayInt[i] + ", ");
+            System.out.print(arrayInt[i] + ", ");
 
         }
         System.out.println("");
@@ -66,29 +69,30 @@ public class Main {
                 System.out.println(arrayDouble[i]);
                 break;
             }
-            System.out.print(arrayDouble[i]+", ");
+            System.out.print(arrayDouble[i] + ", ");
         }
         System.out.println("");
 
-        for (int i = arrayDouble.length; i>=0; i--){
+        for (int i = arrayDouble.length; i >= 0; i--) {
             if (i == 0) {
                 System.out.println(arrayString[i]);
                 break;
             }
-            System.out.print(arrayString[i]+", ");
+            System.out.print(arrayString[i] + ", ");
         }
         System.out.println("");
     }
+
     public static void task4() {
         System.out.println("Task4");
         //int[] arrayInt = new int[] { 1, 2, 3 };
 
-        for(int i: arrayInt){
+        for (int i : arrayInt) {
             if (arrayInt[i] % 2 != 0) {
                 arrayInt[i] += 1;
             }
             if (i == arrayInt.length - 1) {
-                System.out.println(arrayInt[i]+1);
+                System.out.println(arrayInt[i]);
                 break;
             }
             System.out.print(arrayInt[i]", ");
